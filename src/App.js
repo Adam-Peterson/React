d893ff95b17c8d8b58ace638afc6232ced99819c
Adam Peterson <adpete44@gmail.com>
2019-04-03 08:31:51 -0600

    beginning of react app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      'counter': 2,
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Welcome to React Today!</h1>
+          <h2>Your name is {this.props.name}</h2>
+          <h2>The counter is at {this.state.counter}</h2>
+          <button onClick={
+            (e) => {
+              this.setState((state, props) => {
+                  return {
+                    counter: state.counter * 10000
+                  }
+              })
+            }
+          }>Increment Counter</button>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> filler.
           </p>
           <a
             className="App-link"
